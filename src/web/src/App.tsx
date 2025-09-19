2327cb899f5466e0893b285a3e6a4382b8aa85d7
KhoaNguyen <anhkhoa515.dev@gmail.com>
2025-09-20 00:30:57 +0700

    feat: ✨ config a new library

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,12 +1,41 @@
 import { ThemeProvider } from './context/themeContext'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer, Bounce} from 'react-toastify';
+
+
+import Navbar from './components/navbar'
+
+import LandingPage from './pages/landingPage'
+import LoginPage from './pages/loginPage'
+import RegisterPage from './pages/registerPage'
 
 function App() {
 
   return (
     <ThemeProvider> 
-      <div className="App">
-        <h1>Hello, World!</h1>
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+
+        </Routes>
+        <ToastContainer
+                  position='top-right'
+                  autoClose={2000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme='light'
+                  transition={Bounce}
+                />
+        <ToastContainer />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
