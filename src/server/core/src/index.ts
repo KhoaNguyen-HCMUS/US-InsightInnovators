75fbca9f6a53de7e14fa2a5e70cce699bba75f0f
Minh Duy <mduy2204@gmail.com>
2025-09-25 21:25:38 +0700

    feat: ✨ implement food photo feature

--- src/server/core/src/index.ts
+++ src/server/core/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
+import foodRoutes from "./routes/food";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api", foodRoutes);
 
 app.get("/api/health", (_req, res) => res.json({ ok: true }));
 
