6a0bc0750c8add54f252647df5c7398b04570580
Minh Duy <mduy2204@gmail.com>
2025-09-25 21:23:05 +0700

    feat: ✨ implement fool photo model

--- src/server/ml/main.py
+++ src/server/ml/main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from food101.model import classify_food
 
 app = FastAPI()
 
 @app.get("/api/health")
 def health_check():
     return {"ok": True}
+
+@app.post("/predict-food101")
+async def predict_food(file: UploadFile = File(...)):
+    contents = await file.read()
+    result = classify_food(contents)
+    return result
 
 if __name__ == "__main__":
     import uvicorn
